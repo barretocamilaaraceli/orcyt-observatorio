bee46f84743795af7531b49a76c5cf6cbe745289
Camila Barreto <barretocamila.araceli@gmail.com>
2026-01-07 02:38:55 -0300

    Typography: justify text and enable hyphenation

--- site/observablehq.config.js
+++ site/observablehq.config.js
@@ -18,7 +18,7 @@ export default {
 
   head: `
 <link rel="icon" href="favicon.ico">
-<link rel="icon" type="image/png" href="favicon-32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="favicon-32.png" sizes="40x40">
 <link rel="icon" type="image/png" href="favicon-96.png" sizes="96x96">
 <link rel="apple-touch-icon" href="apple-touch-icon.png" sizes="180x180">
 <link rel="stylesheet" href="style.css">
